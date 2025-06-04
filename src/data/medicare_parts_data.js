c8e98d15789e24de47476d1fb93a9d62aec4c519
Justin Patrick <jpatrick6420@gmail.com>
2025-06-04 06:58:28 -0600

    added chapter 4

--- src/data/medicare_parts_data.js
+++ src/data/medicare_parts_data.js
@@ -223,6 +223,198 @@ const questions = [
     answer:
       "Medicare perscription drug plans are not permittted to cover prescription meds the vaugns are interested in nor cosmetic nor vitimins but they might look at an adantage plan that offers them as a supplemental benefit.",
   },
+  {
+    question:
+      "Mr. Wingate is a newly enrolled Medicare Part D beneficiary and one of your clients. In addition to drugs on his plan's formulary, he takes several other medications. These include a prescription drug not on his plan's formulary, over-the-counter medications for colds and allergies, vitamins, and drugs from an Internet-based Canadian pharmacy to promote hair growth and reduce joint swelling. His neighbor recently told him about a concept called TrOOP and he asks you if any of his other medications could count toward TrOOP should he ever reach the Part D catastrophic limit. What should you say?",
+    answer:
+      "Some costs do not count towards Troop. That includes costs for drugs not on a Part D plan's formulary unless the beneficiary receives an exeception under which the plan covers the drug.",
+  },
+  {
+    question:
+      "Mrs. Allen has a rare condition for which two different brand name drugs are the only available treatment. She is concerned that since no generic prescription drug is available and these drugs are very high cost, she will not be able to find a Medicare Part D prescription drug plan that covers either one of them. What should you tell her?",
+    answer:
+      "Part D formularies must include at least two drugs in each theraputic category or not generic versions available. Mrs allen should be able to find a plan that covers a medication that she needs.",
+  },
+  {
+    question:
+      "Ms. Edwards is enrolled in a Medicare Advantage plan that includes prescription drug plan (PDP) coverage. She is traveling and wishes to fill two of the prescriptions that she has lost. How would you advise her?",
+    answer:
+      "She may fill prescriptions for covered drugs at non-network pharmacies but at a higher rate than at in-network pharmacies.",
+  },
+  {
+    question:
+      "Mr. Bickford did not quite qualify for the extra help low-income subsidy under the Medicare Part D Prescription Drug program and he is wondering if there is any other option he has for obtaining help with his considerable drug costs. What should you tell him?",
+    answer:
+      "He could check with the mamufacturer to see if they offer an assistance program to help people of limited means. alternative, he could check with his state and check for an assistance program.",
+  },
+  {
+    question:
+      "Mr. Hildalgo complains to you that because he takes multiple expensive drugs, he has trouble paying his cost sharing for his prescription drugs, particularly at the beginning of the year during the deductible phase. He is happy with his plan and does not want to change. However, he said he had heard about a new program called the Medicare Prescription Payment Plan and asked whether it might help. What do you tell him?",
+    answer:
+      "The medicare prescription payment plan helps spread payments for meds over an entire year.",
+  },
+  {
+    question:
+      "Mr. Carlini has heard that Medicare prescription drug plans are only offered through private companies under a program known as Medicare Advantage (MA), not by the government. He likes Original Medicare and does not want to sign up for an MA product, but he also wants prescription drug coverage. What should you tell him?",
+    answer:
+      "Correct: Mr. Carlini can stay in Original Medicare and obtain prescription drug benefits through a stand-alone Part D plan. He does not have to enroll in a MA plan..",
+  },
+  {
+    question:
+      "What types of tools can Medicare Part D prescription drug plans use that affect the way their enrollees can access medications?",
+    answer:
+      "Part D plans do not have to cover all medications. As a result formularies will very from each other but they must at least be as good as the standard set out by the state.",
+  },
+  {
+    question:
+      "Mr. Zachow has a condition for which three drugs are available. He has tried two but had an allergic reaction to them. Only the third drug works for him and it is not on his Part D plan's formulary. What could you tell him to do?",
+    answer: "Zachow can fill out an exception form.",
+  },
+  {
+    question:
+      "Mrs. Roswell is a new Medicare beneficiary who has just retired from retail work. She is interested in selecting a Medicare Part D prescription drug plan. She takes several medications and is concerned that she has not been able to identify a plan that covers all of her medications. She does not want to make an abrupt change to new drugs that would be covered and asks what she should do. What should you tell her?",
+    answer:
+      "Every part d drug  plan is required to cover a one month fill of her existing medication sometime during the first 90 days.",
+  },
+  {
+    question:
+      "Mr. Torres has a small savings account. He would like to pay for his monthly Part D premiums with an automatic monthly withdrawal from his savings account until it is exhausted, and then have his premiums withheld from his Social Security check. What should you tell him?",
+    answer: "the beneficiary must pick a payment method for a year",
+  },
+  {
+    question:
+      "Mr. Rice is 68, actively working, and has coverage for medical services and medications through his employer's group health plan. He is entitled to premium free Part A and thinking of enrolling in Part B and switching to an MA-PD because he is paying a very large part of his group coverage premium, and it does not provide coverage for a number of his medications. Which of the following is NOT a consideration when making the change?",
+    answer: "Mr. rices retiree plan is not required to take him back",
+  },
+  {
+    question:
+      "Mrs. Mulcahy, age 65, is concerned that she may not qualify for enrollment in a Medicare prescription drug plan because, although she is entitled to Part A, she is not enrolled under Medicare Part B. What should you tell her?",
+    answer:
+      "She can enroll because she is eligible for Part A. She doesn't need to be enrolled in Part B for the drug plan",
+  },
+  {
+    question:
+      "Mrs. Hernandez is one of your clients. She has read that there is a new program that may help her manage prescription drug costs. What do you tell her about the Medicare Prescription Payment Plan?",
+    answer:
+      "Part D enrollees can opt into a prescription payment plan anytime throughout the year",
+  },
+  {
+    question:
+      "Winthrop Brokerage wishes to place an advertisement in the local newspaper that says: 'We offer Medicare Advantage plans offered by AB Health and Top Choice Health. Contact us if you would like to learn more0' .Which of the following best describes the obligation(s) of Winthrop Brokerage regarding the advertisement?",
+    answer:
+      "Winthrop Brokerage does not need to submit the advertisement to CMS because it doesn't include details about the plans benefit structure or copayment structure",
+  },
+  {
+    question:
+      "Next week you will be participating in your first 'educational event' for prospective enrollees. To be sure that you do not violate any of the applicable guidelines, in what activities should you plan to engage?",
+    answer:
+      "Correct: Sales presentations and distribution or acceptance of enrollment forms are prohibited when an event is advertised as educational.",
+  },
+  {
+    question:
+      "Agent jennings makes a presentation on Medicare advertised as an educational event. Agent Jennings distributes materials that are solely educational. However, she gives a brief presentation that mentions plan-specific premiums. Is this a prohibited activity at an event that has been advertised as educational?",
+    answer:
+      "Yes. When an event is advertised as educational, discussing plan specific premiums is impermissable.",
+  },
+  {
+    question:
+      "One of your colleagues argues that it is better to focus your time and energy exclusively on neighborhoods with single-family homes. He further argues that their older owners are more likely to have higher incomes and purchase the Medicare Advantage products you represent compared to those living in apartment complexes. How should you respond?",
+    answer: "This is considered discriminatory and prohibited",
+  },
+  {
+    question:
+      "During a sales presentation to Ms. Daley for a Medicare Advantage plan that has a 5-star rating in customer service and care coordination, and received an overall plan performance rating of a 4-star, which of the following would be the best statement to say to her?",
+    answer:
+      "The medicare advantage plan received a 5 star rating in customer service and care cordination with an ovrall rating of 4 stars",
+  },
+  {
+    question:
+      "By contacting plans avallable in your area, you have learned that the plan you represent has a significantly lower monthly premium than the others. Furthermore, you see that the plan you represent has a unique benefits package. What shouldlyou do to make sure your clients know about these pieces of information?",
+    answer:
+      "You may make comparisons between plans if you can support them with studies or stats and such comparisons are fact based",
+  },
+  {
+    question:
+      "When you market Medicare Advantage and Part D plans, what may you offer as a gift to induce enrollment in a plan?",
+    answer:
+      "You may provide gifts or prices to all potential enrollees during the event that don't exceed 15 dollars",
+  },
+  {
+    question:
+      "If you are to comply with Medicare's guidance regarding educational events, which of the following would be acceptable activities?",
+    answer:
+      "You may distribute business careds to individuals who request information on how to contact you for further details",
+  },
+  {
+    question:
+      "Your colleague works at a third-party marketing organization (TMO) and she said she did not need to take the Medicare training for brokers and agents or pass a test to market Medicare plans since her contract is with the TMO, not the plans that have the products she sells. What could you say to her?",
+    answer:
+      "You cold tell her she is wrong and that only agents selling employeer/union group plans are permitted are exempt from testing but some may still require testing. ",
+  },
+  {
+    question:
+      "You will be holding a sales event soon, at which you would like to offer door prizes to attendees. Under guidelines from the Medicare agency, what types of gifts or prizes would not be allowed in this Situation?",
+    answer:
+      "Gift cards of gift certificates of $ 15 that can be easily converted to cash. ",
+  },
+  {
+    question:
+      "While making an appointment to discuss Medicare Advantage (MA) and Part D plans with a potential enrollee, you are asked to describe other types of insurance products that your client might wish to purchase, What additional types of insurance can you present during the MA and Part D marketing appointments?",
+    answer: "You can present only health care related products ",
+  },
+  {
+    question:
+      "You would like to market a MA plan at a neighborhood pharmacy. What should you keep in mind to comply with the marketing requirements for MA plans?",
+    answer:
+      "Incorrect: Marketing representatives must not engage in marketing activities where patients receive health care services such as a pharmacy counter area. Marketing activities would include accepting enrollment applications.",
+  },
+  {
+    question:
+      "You have set up an appointment for an in home sales presentation with Mrs. Fernandez, who expressed interest in the Medicare plans you represent. In preparation for the sales presentation, What must you do?",
+    answer:
+      "Correct: Before any marketing appointment, you must identify the types of plans that you will discuss as well as obtain an agreement from her and document such agreement through a scope of appointment.",
+  },
+  {
+    question:
+      "You are mailing invitations to new Medicare beneficiaries for a marketing event. You want an idea of how many people to expect, so you would like to request RSVPs. What should you keep in mind?",
+    answer:
+      "Correct: At marketing/sales events, agents may not require beneficiaries to provide contact information as a prerequisite for attending the event. This includes requiring an email address or other contact information as a condition to RSVP for an event.",
+  },
+  {
+    question:
+      "You market many different types of insurance and ordinarily you spend time each evening calling potential clients. To comply with requirements for marketing Medicare Advantage and Part D plans, what must you do about contacting potential clients to market those plansi?",
+    answer:
+      "You will have to avoid calling any potential client unless he or she initiates you a contact with you.",
+  },
+  {
+    question:
+      "Mr. Prentice has many clients who are Medicare beneficiaries. He should review the Centers for Medicare & Medicaid Services (CMS) Communication and Marketing Guidelines to ensure he is compliant with which type of products?",
+    answer: "The CMS communication guidelines apply to MA and PDP plans.",
+  },
+  {
+    question:
+      "Agent Harriet Walker has recently begun marketing Medicare Advantage and related products aimed at meeting the needs of senior citizens. Client Mildred Jones has expressed interest in a Medicare Advantage plan. It is now the beginning of September. If you were in Agent Walker's position, what Would you do?",
+    answer:
+      "Inquire whether the client qualifies for a specific enrollement period and if not solicit an enorollement app once the annual enrollement period begins on oct 15th.",
+  },
+  {
+    question:
+      "You plan to participate in an educational event sponsored by a large regional health care system, One of your colleagues suggests that you do a presentation on one of the Medicare Health plans you market and modity it to include information about preventive screening tests showcased at the event. How should you respond to your colleague's suggestion?",
+    answer:
+      "You should tellyour colleage no because participation in an educational event cannot include a sales presentation",
+  },
+  {
+    question:
+      "A Medicare beneficiary has walked into your office and requested that you sit down with her and discuss her options under the Medicare Advantage program. Before engaging in such a discussion, what should you do?",
+    answer:
+      "you must document your agreement to discuss MA options in the scope of the appointment",
+  },
+  {
+    question:
+      "You are meeting with Mrs. Hall in her home. On her scope of appointment form, she asked to discuss Medicare Advantage plans. During the meeting, she asked to discuss a stand-alone prescription drug plan. She is leaving the next day to visit her family for a week in another state, so she needs to decide before she leaves. What must happen before that additional discussion can take place?",
+    answer:
+      "Since MRs hall specifically asked you to discuss the PDP you can as long as you fill out a new scope of appointment first indicating that she want to talk about it with you.",
+  },
 ];
 
 //part 3 question 8
